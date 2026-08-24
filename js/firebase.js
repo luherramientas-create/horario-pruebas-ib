0c62ce47082cd387894c7f9be24a36700fc701ab
luherramientas-create <luherramientas@gmail.com>
2026-08-24 14:45:50 -0600

    Conectar la aplicación al proyecto Firebase exclusivo Pruebas IB Cariari

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
 import { getFirestore, collection, addDoc, doc, getDoc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
 
+// Proyecto Firebase exclusivo de Horario de Pruebas IB.
 const firebaseConfig = {
-  apiKey: 'AIzaSyCjE7kpwMZcFRVJsJcWPIQwEzgH-YrcXk0',
-  authDomain: 'registro-edu-aa4c8.firebaseapp.com',
-  projectId: 'registro-edu-aa4c8',
-  storageBucket: 'registro-edu-aa4c8.firebasestorage.app',
-  messagingSenderId: '1032924835108',
-  appId: '1:1032924835108:web:f21d00c988d9898b3497b1'
+  apiKey: 'AIzaSyAwzTmyxrRXJJg12E0oWf_ypvH3ZMOGUKA',
+  authDomain: 'pruebas-ib-cariari.firebaseapp.com',
+  projectId: 'pruebas-ib-cariari',
+  storageBucket: 'pruebas-ib-cariari.firebasestorage.app',
+  messagingSenderId: '710955776260',
+  appId: '1:710955776260:web:9b9a9d967de0f1ad76f851'
 };
 
 export const app = initializeApp(firebaseConfig);
@@ -17,4 +18,4 @@ export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 
-export { collection, addDoc, doc, getDoc, serverTimestamp, signInWithPopup, signInWithEmailAndPassword, signOut, onAuthStateChanged };
+export { collection, addDoc, doc, getDoc, serverTimestamp, signInWithPopup, signOut, onAuthStateChanged };
